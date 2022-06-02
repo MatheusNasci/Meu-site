b7c429fe45847f81d0ddf489781807cc928a40bb
MatheusNasci <matheus.noliveira@sptech.school>
2022-06-02 13:53:11 -0300

    API Site Individual

---
+++ site/public/js/funcoes.js
@@ -0,0 +1,59 @@
+// sessão
+function validarSessao() {
+    // aguardar();
+
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+    var tipo = sessionStorage.FK_TIPO;
+
+    var h1LoginUsuario = document.getElementById("h1_login_usuario");
+
+    if (email != null && nome != null) {
+        if (h1LoginUsuario != undefined) {
+            h1LoginUsuario.innerHTML = email;
+        }
+        var username = document.getElementById("b_usuario")
+        username.innerHTML = nome
+        var icone = document.getElementById("nv_li")
+        icone.style.display = "none"
+        // finalizarAguardar();
+    } else {
+        window.location = "../login.html";
+    }
+}
+
+function limparSessao() {
+    // aguardar();
+    sessionStorage.clear();
+    // finalizarAguardar();
+    window.location = "../login.html";
+}
+
+// carregamento (loading)
+function aguardar() {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "flex";
+}
+
+function finalizarAguardar(texto) {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "none";
+
+    var divErrosLogin = document.getElementById("div_erros_login");
+    if (texto) {
+        divErrosLogin.innerHTML = texto;
+    }
+}
+
+
+// modal
+function mostrarModal() {
+    var divModal = document.getElementById("div_modal");
+    divModal.style.display = "flex";
+}
+
+function fecharModal() {
+    var divModal = document.getElementById("div_modal");
+    divModal.style.display = "none";
+}
+
---
+++ site/src/database/script-tabelas.sql
@@ -0,0 +1,16 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/* para workbench - local - desenvolvimento */
+create database portalnether;
+
+use portalnether;
+
+create table usuario(
+    idUsuario int primary key auto_increment,
+    nome varchar(100),
+    email varchar(100),
+    senha varbinary(128)
+);
+
